f1bb731f5a4b55df16832b6834d318fa100235f7
Ayoub RABIE <105998148+Ayoubrabiae@users.noreply.github.com>
2023-06-20 23:49:33 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header>
       <div className="container">
         <div className="logo">
-          <img src="../../public/logo.png" alt="Logo Image" />
+          <img src="../public/logo.png" alt="Logo Image" />
         </div>
         <nav>
           <ul>
