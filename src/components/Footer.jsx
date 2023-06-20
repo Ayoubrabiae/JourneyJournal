65d1c77e55d529bf9906af42d2d27ce63aa99b3a
Ayoub RABIE <105998148+Ayoubrabiae@users.noreply.github.com>
2023-06-20 15:02:32 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-export function Footer() {
+export default function Footer() {
   return (
     <footer>
       © {new Date().getFullYear()} <a href="https://www.linkedin.com/in/ayoub-rabiya-021015244/">rabiyaAyoub</a>
