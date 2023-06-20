5e38b30f8f282c04a5a456aae058b09247612fbb
Ayoub RABIE <105998148+Ayoubrabiae@users.noreply.github.com>
2023-06-20 23:26:39 +0100

    Update BrowseData.jsx

--- src/BrowseData.jsx
+++ src/BrowseData.jsx
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     title: "Morocco Journey",
-    img: "card1.jpg",
+    img: "https://cdn.discordapp.com/attachments/1059547728369156268/1120664011986444339/7abe5077-e9c2-4342-9131-71c2c69280a6.jpg",
     location: "Morocco",
     date: "7/20/2019",
     description: "This a very nice Journy In this beauty country",
@@ -11,7 +11,7 @@ export default [
   {
     id: 2,
     title: "Italiy Journey",
-    img: "card2.jpg",
+    img: "https://cdn.discordapp.com/attachments/1059547728369156268/1120664430871580673/55b11359-4777-45fd-b21d-1fb6912628c3.jpg",
     location: "Italiy",
     date: "9/23/2020",
     description: "This a very nice Journy In this beauty country",
@@ -20,7 +20,7 @@ export default [
   {
     id: 3,
     title: "Spain Journey",
-    img: "card3.jpg",
+    img: "https://cdn.discordapp.com/attachments/1059547728369156268/1120666521119764541/0fbced79-5a0e-4e32-a51b-72f294d3137b.jpg",
     location: "Spain",
     date: "8/13/2021",
     description: "This a very nice Journy In this beauty country",
@@ -29,7 +29,7 @@ export default [
   {
     id: 4,
     title: "Sea Journey",
-    img: "card4.jpg",
+    img: "https://cdn.discordapp.com/attachments/1059547728369156268/1120667182116913204/949c5e22-35bd-4f0a-9ece-10fb8a81d131.jpg",
     location: "Egypt",
     date: "4/20/2023",
     description: "This a very nice Journy In this beauty country",
